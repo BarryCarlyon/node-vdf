d68dea2cde03d69969c8b36e1cae863f3b15aaf7
Ron Melkhior <ron3353@gmail.com>
2017-05-30 04:49:14 +0300

    Remove dump method, and allow duplicate key entries

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ Distributed under the ISC License (see LICENSE)
 Ported to node.js by Rob Jackson - rjackson.me.
 */
 
-var util = require("util");
+const _ = require('lodash');
 
-var STRING = '"',
-    NODE_OPEN = '{',
-    NODE_CLOSE = '}',
-    BR_OPEN = '[',
-    BR_CLOSE = ']',
-    COMMENT = '/',
-    CR = '\r',
-    LF = '\n',
-    SPACE = ' ',
-    TAB = '\t',
-    WHITESPACE = [SPACE, "\t", "\r", "\n"];
+const STRING = '"';
+const NODE_OPEN = '{';
+const NODE_CLOSE = '}';
+const BR_OPEN = '[';
+const BR_CLOSE = ']';
+const COMMENT = '/';
+const CR = '\r';
+const LF = '\n';
+const SPACE = ' ';
+const TAB = '\t';
+const WHITESPACE = [SPACE, '\t', '\r', '\n'];
 
 function _symtostr(line, i, token) {
     token = token || STRING;
@@ -65,25 +65,24 @@ function _parse(stream, ptr) {
 
         if (c === NODE_OPEN) {
             next_is_value = false;  // Make sure the next string is interpreted as a key.
-
             var parsed = _parse(stream, i + 1);
-            deserialized[laststr] = parsed[0];
+
+            if (!deserialized[laststr])
+                deserialized[laststr] = {};
+                
+            _.merge(deserialized[laststr], parsed[0]);
             i = parsed[1];
-        }
-        else if (c === NODE_CLOSE) {
+        } else if (c === NODE_CLOSE) {
             return [deserialized, i];
-        }
-        else if (c === BR_OPEN) {
+        } else if (c === BR_OPEN) {
             var _string = _symtostr(stream, i, BR_CLOSE);
             lastbrk = _string[0];
             i = _string[1];
-        }
-        else if (c === COMMENT) {
+        } else if (c === COMMENT) {
             if ((i + 1) < stream.length && stream.substring(i + 1, i + 2) === "/") {
                 i = stream.indexOf("\n", i);
             }
-        }
-        else if (c === CR || c === LF) {
+        } else if (c === CR || c === LF) {
             var ni = i + 1;
             if (ni < stream.length && stream.substring(ni, ni + 1) === LF) {
                 i = ni;
@@ -91,8 +90,7 @@ function _parse(stream, ptr) {
             if (lasttok != LF) {
                 c = LF;
             }
-        }
-        else if (c !== SPACE && c !== TAB) {
+        } else if (c !== SPACE && c !== TAB) {
             var _string = (c === STRING ? _symtostr : _unquotedtostr)(stream, i);
             string = _string[0];
             i = _string[1];
@@ -108,8 +106,7 @@ function _parse(stream, ptr) {
             c = STRING;  // Force c == string so lasttok will be set properly.
             laststr = string;
             next_is_value = !next_is_value;
-        }
-        else {
+        } else {
             c = lasttok;
         }
 
@@ -120,46 +117,9 @@ function _parse(stream, ptr) {
     return [deserialized, i];
 }
 
-function _dump(obj, indent, mult) {
-    indent = indent || 0,
-    mult = mult || 2;
-
-    function _i() {
-        return Array(indent * mult + 1).join(" ");
-    }
-
-    var nodefmt = '\n' + _i() + '"%s"\n' + _i() + '{\n%s' + _i() + '}\n\n',
-        podfmt = _i() + '"%s" "%s"\n',
-        lstfmt = _i() + (Array(mult + 1).join(" ")) + '"%s" "1"';
-
-    indent += 1;
-
-    var nodes = [];
-
-    for (var k in obj) {
-        var v = obj[k];
-        if (typeof v === 'object' && !(v instanceof Array)) {
-            nodes.push(util.format(nodefmt, k, _dump(v, indent, mult)));
-        }
-        else if (v instanceof Array) {
-            lst = v.map(function(more_v){ return util.format(lstfmt, more_v); });
-            nodes.push(util.format(nodefmt, k, lst.join("\n") + '\n'));
-        }
-        else {
-            nodes.push(util.format(podfmt, k, v));
-        }
-    }
-
-    indent -= 1;
-    return nodes.join("");
-}
-exports.parse = function parse(string) {
+module.exports.parse = function parse(string) {
     var _parsed = _parse(string);
     res = _parsed[0];
     ptr = _parsed[1];
     return res;
-};
-
-exports.dump = function dump(obj) {
-    return _dump(obj);
 };
